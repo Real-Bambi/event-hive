a93622a898cf5aaa201e044bfaa1288004e767f1
Sally <salomeytackie555@gmail.com>
2025-06-15 18:20:32 +0000

    completion of login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,36 @@
-export default function Register() {
+  import GoogleImage from '../assets/images/google-icon.png'
+  export default function Login() {
     return (
-        <div>
-            <h1>Welcome to the Register Page</h1>
+        <div className="flex h-screen ">
+           <div className="w-3/5 flex flex-col pt-16 bg-bgGrey ">
+        <h1 className="font-bold text-2xl text-center">Event <span className= "text-primary">Hive</span></h1>
+        <h2 className="text-4xl text-center font-bold pt-10">Sign In for Event Hive</h2>
+        <form className='flex flex-col pt-16 w-[80%] mx-auto '>
+            <div className='flex flex-col pt-2 '>
+                <label htmlFor="your name" className='p-2 font-medium'>YOUR EMAIL</label>
+            <input type="text" name="name" id="name" placeholder="Enter your mail" className='w-[100%] px-6 py-2 bg-white ' />
+            </div>
+            <div className='flex flex-col pt-8 '>
+                <div className='flex justify-between items-center mb-2'>
+                    <label htmlFor="password" className='font-medium' >PASSWORD</label>
+                    <a href="#" className='font-light'>forgot password?</a>
+                </div>
+                <input type="password" name="password" id="passowrd" placeholder="Enter your password" className='w-[100%] px-6 py-2 bg-white' />
+            </div>
+            <div className='flex flex-col items-center justify-center pt-10 '>
+                <button className='text-white bg-primary w-48 px-6 py-2  rounded  '>Sign In</button>
+                <p className='text-center font-light p-8 '>Or</p>
+                <button className='bg-white items-center pt-4 px-6 py-2 rounded w-64 shadow gap-4'> <img src={GoogleImage} alt="google-icon" className='align-middle inline-block w-5 h-5  ' />  Sign Up with Google</button>
+            </div>
+        </form>
+            </div> 
+            <div className='w-2/5 bg-[url(./assets/images/login.png)] bg-center bg-cover '>
+                <div className='flex flex-col items-center justify-center h-[100vh]'>
+                    <h1 className='text-white font-bold text-3xl'>Hello Friend</h1>
+                    <p className='text-white p-8 '>To keep connected with us provide us with your information</p>
+                    <button className='text-white bg-lightWhite pt-4 px-6 py-2 rounded '>Sign Up</button>
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import GoogleImage from '../assets/images/google-icon.png'
 export default function Register() {
     return (
-        <div className=" mx-auto mt-[80px] h-screen flex flex-col md:flex-row justify-around">
+        <div className="  h-screen flex flex-col md:flex-row justify-around">
             <div className="bg-[url(./assets/images/register.png)]  bg-cover bg-center w-2/5 ">
                 <div className="text-white h-[90vh]  flex flex-col items-center justify-center">
                     <h1 className="text-4xl font-bold">Welcome back</h1>
@@ -30,9 +30,9 @@ export default function Register() {
                         <input password="text" name="name" id="name" className="w-[100%] px-4 py-2 bg-white" placeholder="Enter your password" />
                         </div>
              <div className='flex flex-col items-center'>
-                <button className="bg-primary px-6 py-2 w-[50%] flex items-center justify-center text-white rounded mt-8">Sign up</button>
+                <button className="bg-primary px-6 py-2 w-48 flex items-center justify-center text-white rounded mt-8">Sign up</button>
               <p className="pt-4 font-light text-pgray">Or</p>
-              <button className=' pt-4  border w-[70%] rounded bg-white py-2 px-20 gap-4'> <img src={GoogleImage} alt="google icon"  className='w-5 h-5 align-middle inline-block'/> Sign Up with  Google</button>
+              <button className='shadow w-80 mt-4 rounded bg-white py-2 px-4 gap-4'> <img src={GoogleImage} alt="google icon"  className='w-5 h-5 align-middle inline-block'/> Sign Up with  Google</button>
               
             
              </div>
