c1649e5bf3aeab3153c2c59cc5dcb023badf882d
Dede <dedenyengor09@gmail.com>
2025-06-16 12:05:38 +0000

    viewcollege commit

--- src/components/CollegeEvents.jsx
+++ src/components/CollegeEvents.jsx
@@ -4,12 +4,12 @@ export default function CollegeEvents() {
     return (
         <section className="mt-20 w-[90%] mx-auto">
             <div>
-                <h1>
-                    <span>College</span>
-                    <span>Events</span>
+                <h1 className="pb-6 flex gap-3">
+                    <span className="font-bold text-4xl text-black">College</span>
+                    <span className="font-bold text-4xl text-[#7848F4]">Events</span>
                 </h1>
             </div>
-            <div className="grid grid-cols-3 gap-5">
+            <div className="grid grid-cols-3 gap-5 ">
                 {[1, 2, 3, 4, 5, 6].map(n => <EventCard key={n} />)}
             </div>
         </section>
