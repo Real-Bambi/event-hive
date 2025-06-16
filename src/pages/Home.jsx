6b1418576d7df48bc1057abec2e09b216e9768e2
King-Cyrus00 <cyrus.barrow07@gmail.com>
2025-06-16 11:32:38 +0100

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,6 +14,7 @@ import Stripe from "../assets/images/stripe.png"
 import Uber from "../assets/images/uber.png"
 import Youtube from "../assets/images/youtube.png"
 import Zoom from "../assets/images/zoom.png"
+import { Link } from "react-router";
 
 
 export default function Home() {
