5e12e3e310668d3d28f24eefa8e7fa13a8b49905
Sally <salomeytackie555@gmail.com>
2025-06-17 11:35:43 +0000

    complete integration in register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,26 @@
-import GoogleImage from '../assets/images/google-icon.png'
+import SubmitButton from '../components/SubmitButton';
+import GoogleImage from '../assets/images/google-icon.png';
+import { apiClient } from '../api/client';
+import { useNavigate } from 'react-router';
+
 export default function Register() {
+    const navigate = useNavigate();
+
+    const registerUser = async (data) => {
+        try {
+            const response = await apiClient.post('/users/register', data,{
+                headers: {
+                    "Content-Type": 'application/json',
+                }
+            });
+            console.log(response);
+            navigate("/login");
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
     return (
         <div className="  h-screen flex flex-col md:flex-row justify-around">
             <div className="bg-[url(./assets/images/register.png)]  bg-cover bg-center w-2/5 ">
@@ -12,31 +33,48 @@ export default function Register() {
             <div className="w-3/5 items-center justify-center bg-bgGrey">
                 <h1 className="text-2xl font-bold text-center">Event <span className="text-purple-600">Hive</span></h1>
                 <h2 className="text-3xl font-medium p-4 text-center">Sign Up to Event Hive</h2>
-                <form action className="w-[60%] mx-auto">
+                <form action={registerUser} className="w-[60%] mx-auto">
                     <div className="flex flex-col p-2  ">
                         <label htmlFor="your name" className="font-medium p-2">YOUR NAME</label>
-                        <input type="text" name="name" id="name" className="w-[100%] px-4 py-2 bg-white" placeholder="Enter your name" />
+                        <input
+                         type="text"
+                          name="name"
+                           id="name"
+                         className="w-[100%] px-4 py-2 bg-white" placeholder="Enter your name" 
+                         />
                     </div>
                     <div className="flex flex-col p-2 ">
-                        <label htmlFor="your name" className="font-medium p-2">YOUR NAME</label>
-                        <input type="text" name="name" id="name" className="w-[100%] px-4 py-2 bg-white" placeholder="Enter your name" />
+                        <label htmlFor="your email" className="font-medium p-2">YOUR Email</label>
+                        <input
+                         type="email" 
+                         name="email"
+                          id="email"
+                         className="w-[100%] px-4 py-2 bg-white" placeholder="Enter your name"
+                          />
                     </div>
                     <div className="flex flex-col p-2 ">
                         <label htmlFor="your password" className="font-medium p-2">PASSWORD</label>
-                        <input password="text" name="name" id="name" className="w-[100%] px-4 py-2 bg-white" placeholder="Enter your password" />
+                        <input
+                         type="password"
+                          name="password"
+                           id="password" 
+                           className="w-[100%] px-4 py-2 bg-white" placeholder="Enter your password"
+                            />
                     </div>
-                    <div className="flex flex-col p-2 ">
+                    {/* <div className="flex flex-col p-2 ">
                         <label htmlFor="confirm password" className="font-medium p-2">CONFIRM PASSWORD</label>
-                        <input password="text" name="name" id="name" className="w-[100%] px-4 py-2 bg-white" placeholder="Enter your password" />
-                        </div>
-             <div className='flex flex-col items-center'>
-                <button className="bg-primary px-6 py-2 w-48 flex items-center justify-center text-white rounded mt-4">Sign up</button>
-              <p className="pt-4 font-light text-pgray">Or</p>
-              <button className='shadow w-80 mt-3 rounded bg-white py-2 px-4 gap-4'> <img src={GoogleImage} alt="google icon"  className='w-5 h-5 align-middle inline-block'/> Sign Up with  Google</button>
-              
-            
-             </div>
-                </form>
+                        <input password="text" name="password" id="password" className="w-[100%] px-4 py-2 bg-white" placeholder="Enter your password" />
+                    </div> */}
+                    <div className='flex flex-col items-center'>
+                        <SubmitButton
+                            className="bg-primary px-6 py-2 w-48 flex items-center justify-center text-white rounded mt-4" title={"Sign Up"} />
+                            </div>
+                           </form>
+                           <div className='flex flex-col items-center'>
+                        <p className="pt-4 font-light text-pgray">Or</p>
+                        <button className='shadow w-80 mt-3 rounded bg-white py-2 px-4 gap-4'> <img src={GoogleImage} alt="google icon" className='w-5 h-5 align-middle inline-block' /> Sign Up with  Google</button>
+                    </div>
+             
             </div>
         </div>
     );
