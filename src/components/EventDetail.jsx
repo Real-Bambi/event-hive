820f8b630ef51aebaf4f40c91e771be913b215f2
King-Cyrus00 <cyrus.barrow07@gmail.com>
2025-06-18 13:16:00 +0100

    update

--- src/components/EventDetail.jsx
+++ src/components/EventDetail.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Image } from 'lucide-react';
 import { Link } from 'react-router';
+import facebookIcon from '../assets/images/facebook.png'
+import linkedinIcon from '../assets/images/linkedin.png'
+import twitterIcon from '../assets/images/twitter.png'
+import youtube from '../assets/images/youtube.png'
 
 
 
@@ -47,27 +51,28 @@ export default function EventDetail() {
                 {/*left side  */}
                 <div className='w-[600px] flex flex-col gap-5 pt-10 pl-20'>
                     <div>
-                        <h1>Description</h1>
-                        <p>
-                            DesignHub organized a 3D Modeling Workshop using Blender on 16th February at 5 PM. The workshop taught participants the magic of creating stunning 3D models and animations using Blender.
+                        <h1 className='text-heading pb-5 font-bold'>Description</h1>
+                        <p className='text-gray-500'>
+                            DesignHub organized a 3D Modeling Workshop using Blender on 16th February at 5 PM. The workshop taught participants the magic of creating stunning 3D models and animations using Blender. <br />
+                            <br />
 
                             DesignHub organized a 3D Modeling Workshop using Blender on 16th February at 5 PM. The workshop taught participants the magic of creating stunning 3D models and animations using Blender.</p>
                     </div>
-                    <div>
-                        <h2>Hours</h2>
-                        <span>Weekdays hour: 7PM - 10PM</span>
-                        <span> Sunday hour:  7PM - 10PM</span>
+                    <div className='flex flex-col gap-4'>
+                        <h2 className='text-heading font-bold'>Hours</h2>
+                        <span className='text-gray-500'>Weekdays hour: <span className='text-primary'> 7PM - 10PM</span></span>
+                        <span className='text-gray-500'> Sunday hour:  <span className='text-primary'> 7PM - 10PM</span></span>
                     </div>
-                    <div>
-                        <h2>Organizer Contact</h2>
-                        <Link>Please go to www.sneakypeeks.com and refer the FAQ section for more detail</Link>
+                    <div className='gap-3 flex flex-col'>
+                        <h2 className='text-heading font-bold'>Organizer Contact</h2>
+                        <Link className='text-gray-500'>Please go to www.sneakypeeks.com and refer the FAQ section for more detail</Link>
                     </div>
 
                 </div>
                 {/* right side */}
-                <div className='flex flex-col gap-5'>
-                    <div>
-                        <h2>Event Location</h2>
+                <div className='flex flex-col gap-5 pr-20 pt-10 '>
+                    <div className='flex flex-col'>
+                        <h2 className='text-heading font-bold pb-5 '>Event Location</h2>
                         <iframe
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.799747781828!2d-0.22541772603056248!3d5.596579733242414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf9a29c0df5a4f%3A0x95876935d7d102f9!2sGhana%20Communication%20Technology%20University!5e0!3m2!1sen!2sgh!4v1750240614551!5m2!1sen!2sgh"
                             width="100%"
@@ -78,28 +83,23 @@ export default function EventDetail() {
                             referrerPolicy="no-referrer-when-downgrade"
                             title="GCTU Map"
                         ></iframe>
-                        <h3>Dream world wide in jakatra</h3>
-                        <p>This is the location of GCTU capus, Accra, Tesano.</p>
+                        <h3 className='text-[20px] pt-5 pb-3 text-gray-700'>Dream world wide in jakatra</h3>
+                        <p className='text-gray-500'>This is the location of GCTU capus, Accra, Tesano.</p>
                     </div>
                     <div>
-                        <h2>Tags</h2>
-                        <p>
-                            Indonesia event
-                            Jaskaran event
-                            UI
-                            Jaskaran event
-                            Seminar
-                            Jaskaran event
+                        <h2 className='text-heading font-bold pb-3'>Tags</h2>
+                        <p className='text-gray-500'>
+                            Indonesia event         Jaskaran event       UI        Jaskaran event         Seminar           Jaskaran event
                         </p>
                     </div>
                     <div className='flex'>
-                        <h3>Share with friends</h3>
-                        </div>
-                        <div className='flex flex-row'>
-                        <img src="./assets/images/facebook.png" alt="facebook" srcset="" />
-                        <img src="./assets/images/twitter.png" alt="twitter" srcset="" />
-                        <img src="./assets/images/linkedin.png" alt="linkedin" srcset="" />
-                        <img src="./assets/images/youtube.png" alt="youtube" srcset="" />
+                        <h3 className='text-[20px] font-bold'>Share with friends</h3>
+                    </div>
+                    <div className='flex flex-row gap-5'>
+                        <img src={facebookIcon} alt="facebook" srcset="" />
+                        <img src={linkedinIcon} alt="linkedin" srcset="" />
+                        <img src={twitterIcon} alt="twitter" srcset="" />
+
                     </div>
 
                 </div>
