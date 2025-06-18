72cde1b5721db7db864c06594fdb7a6a873ae7c5
Genevieve <genevieveappia2@gmail.com>
2025-06-18 14:58:49 +0000

    trending-integration

--- src/api/client.js
+++ src/api/client.js
@@ -2,4 +2,12 @@ import axios from "axios";
 
 export const apiClient = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL
-});
+}); 
+
+export const apiFetcher = async (url) => {
+    const response = await apiClient.get(url);
+    return response.data;
+    
+}
+
+export const imageBaseURL = import.meta.env.VITE_IMAGE_BASE_URL;
