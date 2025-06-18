33982cfe778743f7c96864126af631d9a1168290
King-Cyrus00 <cyrus.barrow07@gmail.com>
2025-06-18 11:41:18 +0100

    update

--- src/components/EventDetail.jsx
+++ src/components/EventDetail.jsx
@@ -1,22 +1,110 @@
+import React from 'react';
+import { Image } from 'lucide-react';
+import { Link } from 'react-router';
+
+
+
 export default function EventDetail() {
     return (
-        <div className="flex flex-col gap-[40px] bg-gray-50 min-h-[690px] w-[813px] mx-auto  ">
-            <h2 className="text-big-heading font-bold items-center flex flex-col pt-6">Event Description</h2>
-            <div>
-                <label className="text-sm mb-1" htmlFor="">Event Image</label>
-                < input className="min-h-[254px] bg-gray-200 rounded-lg w-full" type="" accept="" placeholder="" />
+        <>
+            <div className="flex bg-[url('./assets/images/event.png')] bg-cover bg-center w-[1320px] min-h-[595px]mx-auto justify-center" >
+                <div className='flex flex-row justify-between pb-[100px]'>
+                    <div className=' '>
+                        <div className='pl-[60px] pt-[30px] '>
+                            <Link to={-1} className='rounded-[5px] bg-primary p-[10px] w-[85px] h-[40px] text-white '>Back</Link>
+                        </div>
+
+                        <div className='flex flex-col w-[600px] h-[397px] pl-[60px] pt-[60px] gap-5'>
+                            <h2 className='text-white font-bold text-big-heading'>Dream world wide in jakatra</h2>
+                            <h3 className='text-white font-bold text-heading'>IIIT Sonepat</h3>
+                            <p className='text-sm text-white '>DesignHub organized a 3D Modeling Workshop using Blender on 16th February at 5 PM. The workshop taught participants the magic of creating stunning 3D models and animations using Blender. It was suitable for both beginners and experienced users. The event was followed by a blender-render competition, which added to the excitement.</p>
+                            <span className='text-white'>View map</span>
+                        </div>
+
+                    </div>
+
+
+                    <form className='pt-[100px] pl-[240px] mx-auto items-right' >
+                        <div className='w-[385px] min-h-[338px] bg-white  rounded-[10px] mx-auto flex flex-col gap-6 justify-center'>
+                            <label className='text-black font-bold item-left pl-6 text-sub-heading' htmlFor="">Date & time</label>
+                            <input className='w-[200px] pl-6 text-gray-500' type='datetime-local' name="date&time" id="" />
+                            <p className='text-sm text-primary pl-6'>Add to Calender</p>
+
+                            <div className='flex flex-col gap-2 items-center'>
+                                <button type="submit" className='bg-primary w-[340px] p-2 text-white rounded-[10px]'>Book now</button>
+                                <button type="" className='bg-gray-400 p-2 text-white rounded-[10px] w-[340px]'>Program promoter</button>
+                            </div>
+
+                            <div className='flex items-center mx-auto'>No Refunds</div>
+                        </div>
+
+                    </form>
+                </div>
             </div>
 
-            <div>
-                <label className="text-sm mb-1" htmlFor="">Event Description</label>
-                <textarea
-                    id="description"
-                    placeholder="Type here..."
-                    class="w-full h-[173px] p-3 border border-gray-100 rounded-md placeholder:text-gray-400 resize-none bg-white "
-                ></textarea>
-            </div>
-           
 
-        </div>
+            <div className='flex flex-row justify-between'>
+                {/*left side  */}
+                <div className='w-[600px] flex flex-col gap-5 pt-10 pl-20'>
+                    <div>
+                        <h1>Description</h1>
+                        <p>
+                            DesignHub organized a 3D Modeling Workshop using Blender on 16th February at 5 PM. The workshop taught participants the magic of creating stunning 3D models and animations using Blender.
+
+                            DesignHub organized a 3D Modeling Workshop using Blender on 16th February at 5 PM. The workshop taught participants the magic of creating stunning 3D models and animations using Blender.</p>
+                    </div>
+                    <div>
+                        <h2>Hours</h2>
+                        <span>Weekdays hour: 7PM - 10PM</span>
+                        <span> Sunday hour:  7PM - 10PM</span>
+                    </div>
+                    <div>
+                        <h2>Organizer Contact</h2>
+                        <Link>Please go to www.sneakypeeks.com and refer the FAQ section for more detail</Link>
+                    </div>
+
+                </div>
+                {/* right side */}
+                <div className='flex flex-col gap-5'>
+                    <div>
+                        <h2>Event Location</h2>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.799747781828!2d-0.22541772603056248!3d5.596579733242414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf9a29c0df5a4f%3A0x95876935d7d102f9!2sGhana%20Communication%20Technology%20University!5e0!3m2!1sen!2sgh!4v1750240614551!5m2!1sen!2sgh"
+                            width="100%"
+                            height="300"
+                            style={{ border: 0 }}
+                            allowFullScreen
+                            loading="lazy"
+                            referrerPolicy="no-referrer-when-downgrade"
+                            title="GCTU Map"
+                        ></iframe>
+                        <h3>Dream world wide in jakatra</h3>
+                        <p>This is the location of GCTU capus, Accra, Tesano.</p>
+                    </div>
+                    <div>
+                        <h2>Tags</h2>
+                        <p>
+                            Indonesia event
+                            Jaskaran event
+                            UI
+                            Jaskaran event
+                            Seminar
+                            Jaskaran event
+                        </p>
+                    </div>
+                    <div className='flex'>
+                        <h3>Share with friends</h3>
+                        </div>
+                        <div className='flex flex-row'>
+                        <img src="./assets/images/facebook.png" alt="facebook" srcset="" />
+                        <img src="./assets/images/twitter.png" alt="twitter" srcset="" />
+                        <img src="./assets/images/linkedin.png" alt="linkedin" srcset="" />
+                        <img src="./assets/images/youtube.png" alt="youtube" srcset="" />
+                    </div>
+
+                </div>
+            </div>
+
+        </>
     );
 }
--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -50,7 +50,25 @@ export default function CreateEvent() {
 
   
 </div>
-                <EventDetail />
+                
+                 <div className="flex flex-col gap-[40px] bg-gray-50 min-h-[690px] w-[813px] mx-auto  ">
+            <h2 className="text-big-heading font-bold items-center flex flex-col pt-6">Event Description</h2>
+            <div>
+                <label className="text-sm mb-1" htmlFor="">Event Image</label>
+                < input className="min-h-[254px] bg-gray-200 rounded-lg w-full" type="" accept="" placeholder="" />
+            </div>
+
+            <div>
+                <label className="text-sm mb-1" htmlFor="">Event Description</label>
+                <textarea
+                    id="description"
+                    placeholder="Type here..."
+                    class="w-full h-[173px] p-3 border border-gray-100 rounded-md placeholder:text-gray-400 resize-none bg-white "
+                ></textarea>
+            </div>
+           
+
+        </div>
               <button className="bg-primary h-[49px] w-[813px] rounded-md text-white mx-auto flex flex-col justify-center" type="submit ">Create Event</button>   
             </section>
         </>
