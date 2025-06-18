7d1ad17865c85fd4a3bad07671bf23bfe616c225
Dede <dedenyengor09@gmail.com>
2025-06-18 16:08:30 +0000

    Create event integration one

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -1,76 +1,92 @@
 import EventDetail from "../components/EventDetail";
 import Navbar from "../components/Navbar";
+import useSWR from "swr";
+import { apiFetcher } from "../api/client";
 
 export default function CreateEvent() {
-    return (
-        <>
-            <Navbar />
-            <section className="bg-gray-50  ">
+  const {data} = useSWR("/colleges" , apiFetcher);
 
-                <div className=" ">
-  <h1 className="text-big-heading font-bold text-center mb-4 pt-16">Create Event</h1>
 
-  <form className=" w-[816px] min-h-[460px] flex flex-col p-5 mx-auto gap-[33px] ">
-  
-    <div>
-      <label className=" text-sm mb-2">Event Title</label>
-      <input type="text" placeholder="Enter your mail" className="w-full p-3 rounded-md  bg-white" />
-    </div>
 
-   
-    <div>
-      <label className=" text-sm mb-2">Event Venue</label>
-      <input type="text" placeholder="Enter your mail" className="w-full p-3 rounded-md  bg-white" />
-    </div>
 
-    
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-      <div>
-        <label className=" text-sm mb-2">Start time</label>
-        <input type="text" placeholder="Enter your mail" className="w-full p-3 rounded-md  bg-white" />
-      </div>
-      <div>
-        <label className=" text-sm mb-2">End time</label>
-        <input type="text" placeholder="Enter your mail" className="w-full p-3 rounded-md  bg-white" />
-      </div>
-    </div>
+  return (
+    <>
+      <Navbar />
+      <section className="bg-gray-50  ">
 
-   
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-      <div>
-        <label className=" text-sm mb-2">Start date</label>
-        <input type="text" placeholder="Enter your mail" className="w-full p-3 rounded-md  bg-white" />
-      </div>
-      <div>
-        <label className=" text-sm mb-2">End date</label>
-        <input type="text" placeholder="Enter your mail" className="w-full p-3 rounded-md bg-white" />
-      </div>
-    </div>
-  </form>
+        <div className=" ">
+          <h1 className="text-big-heading font-bold text-center mb-4 pt-16">Create Event</h1>
 
-  
-</div>
-                
-                 <div className="flex flex-col gap-[40px] bg-gray-50 min-h-[690px] w-[813px] mx-auto  ">
-            <h2 className="text-big-heading font-bold items-center flex flex-col pt-6">Event Description</h2>
+          <form className=" w-[816px] min-h-[460px] flex flex-col p-5 mx-auto gap-[33px] ">
+
             <div>
-                <label className="text-sm mb-1" htmlFor="">Event Image</label>
-                < input className="min-h-[254px] bg-gray-200 rounded-lg w-full" type="" accept="" placeholder="" />
+              <label className=" text-sm mb-2">College</label>
+              <select name="college" id="" className="w-full p-3 rounded-md  bg-white">
+                {data?.data.map((college)=> {
+                  return <option key={college.id} value={college.id}> {college.name}</option>
+                })}
+              </select>
             </div>
 
             <div>
-                <label className="text-sm mb-1" htmlFor="">Event Description</label>
-                <textarea
-                    id="description"
-                    placeholder="Type here..."
-                    class="w-full h-[173px] p-3 border border-gray-100 rounded-md placeholder:text-gray-400 resize-none bg-white "
-                ></textarea>
+              <label className=" text-sm mb-2">Event Title</label>
+              <input type="email" placeholder="Enter your mail" className="w-full p-3 rounded-md  bg-white" />
             </div>
-           
+
+
+            <div>
+              <label className=" text-sm mb-2">Event Venue</label>
+              <input type="text" placeholder="Enter your mail" className="w-full p-3 rounded-md  bg-white" />
+            </div>
+
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <div>
+                <label className=" text-sm mb-2">Start time</label>
+                <input type="text" placeholder="Enter your mail" className="w-full p-3 rounded-md  bg-white" />
+              </div>
+              <div>
+                <label className=" text-sm mb-2">End time</label>
+                <input type="text" placeholder="Enter your mail" className="w-full p-3 rounded-md  bg-white" />
+              </div>
+            </div>
+
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <div>
+                <label className=" text-sm mb-2">Start date</label>
+                <input type="text" placeholder="Enter your mail" className="w-full p-3 rounded-md  bg-white" />
+              </div>
+              <div>
+                <label className=" text-sm mb-2">End date</label>
+                <input type="text" placeholder="Enter your mail" className="w-full p-3 rounded-md bg-white" />
+              </div>
+            </div>
+          </form>
+
 
         </div>
-              <button className="bg-primary h-[49px] w-[813px] rounded-md text-white mx-auto flex flex-col justify-center" type="submit ">Create Event</button>   
-            </section>
-        </>
-    );
+
+        <div className="flex flex-col gap-[40px] bg-gray-50 min-h-[690px] w-[813px] mx-auto  ">
+          <h2 className="text-big-heading font-bold items-center flex flex-col pt-6">Event Description</h2>
+          <div>
+            <label className="text-sm mb-1" htmlFor="">Event Image</label>
+            < input className="min-h-[254px] bg-gray-200 rounded-lg w-full" type="" accept="" placeholder="" />
+          </div>
+
+          <div>
+            <label className="text-sm mb-1" htmlFor="">Event Description</label>
+            <textarea
+              id="description"
+              placeholder="Type here..."
+              class="w-full h-[173px] p-3 border border-gray-100 rounded-md placeholder:text-gray-400 resize-none bg-white "
+            ></textarea>
+          </div>
+
+
+        </div>
+        <button className="bg-primary h-[49px] w-[813px] rounded-md text-white mx-auto flex flex-col justify-center" type="submit ">Create Event</button>
+      </section>
+    </>
+  );
 }
