8ea91a7020168f1855a9cf011f6fe03101549054
King-Cyrus00 <cyrus.barrow07@gmail.com>
2025-06-23 15:48:22 +0100

    integration on view-event

--- src/components/CollegeCard.jsx
+++ src/components/CollegeCard.jsx
@@ -1,9 +1,13 @@
-export default function CollegeCard() {
+import { Link } from "react-router";
+import { imageBaseURL } from "../api/client";
+
+export default function CollegeCard({college}) {
     return (
         <div className="border-gray-700 bg-[#EDC7B8] shadow-2xl">
-            <img src={'https://images.unsplash.com/photo-1603437119287-4a3732b685f9?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'} alt="College Card" />
-            <h1 className="text-lg font-bold p-2">Harvard University</h1>
-            <h2 className="text-[12px] italic p-2 mb-2">Cambridge, Massachusetts, UK</h2>
+            <img src={`${imageBaseURL}/${college.image}`} alt={college.name} className="rounded-md" />
+            <h1 className="text-lg font-bold p-2">{college.name}</h1>
+            <h2 className="text-[12px] italic p-2 mb-2">{college.address}</h2>
+            <Link to="/view-college"><button >View College</button></Link>
         </div>
     );
 }
